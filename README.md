7e5241881c409ded08d9de6ed983e34399cfcc7f
neandertalecj <neandertalecj@gmail.com>
2019-09-19 12:37:19 +0300

    I have set the ability convert scss into css from 'galp watch' comand

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ npm install --save-dev gulp
 gulp -v
 
 Create gulpfile.js
+
+Go to the Plugins and looking for "gulp-sass" plugin
+npm install gulp-sass --save-dev
+
+gulp scss - It execute the task that have name "scss"
+gulp watch
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,13 @@
-let gulp = require('gulp');
+let gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+gulp.task('scss', function() {
+    return gulp.src('app/scss/**/*.scss') //.(scss|sass)
+        // .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('watch', function() {
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'))
+})
